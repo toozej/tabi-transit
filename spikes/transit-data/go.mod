671b815573ae6d1dc24aef3bdf16959a8fe3a3b2
James Tooze <toozej@gmail.com>
2026-08-21 11:23:20 -0700

    chore: update Go to 1.27

--- spikes/transit-data/go.mod
+++ spikes/transit-data/go.mod
@@ -1,6 +1,6 @@
 module github.com/toozej/tabi-transit/spikes/transit-data
 
-go 1.26.6
+go 1.27.0
 
 require (
 	github.com/MobilityData/gtfs-realtime-bindings/golang/gtfs v1.0.0
--- tools/toolchain/version_test.go
+++ tools/toolchain/version_test.go
@@ -5,8 +5,8 @@ import "testing"
 func TestIsSupportedGoVersion(t *testing.T) {
 	t.Parallel()
 
-	if !IsSupportedGoVersion("go1.26.6") {
-		t.Fatal("expected Go 1.26 patch release to be supported")
+	if !IsSupportedGoVersion("go1.27.6") {
+		t.Fatal("expected Go 1.27 patch release to be supported")
 	}
 	if IsSupportedGoVersion("go1.25.9") {
 		t.Fatal("expected a different Go minor release to be unsupported")
